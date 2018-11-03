bcaecff21eae8f1261b064f62c7d2b42beae53a5
wblachowski <wojciech@blachowski.pl>
2018-11-03 16:11:21 +0100

    Add mapview

---
+++ app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
@@ -0,0 +1,37 @@
+package com.wblachowski.sportivate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by wblachowski on 11/3/2018.
+ */
+
+public class EventsMapFragment extends Fragment implements OnMapReadyCallback {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
--- app/src/main/java/com/wblachowski/sportivate/MainActivity.java
+++ app/src/main/java/com/wblachowski/sportivate/MainActivity.java
@@ -2,6 +2,7 @@ package com.wblachowski.sportivate;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -65,13 +66,14 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager fm = getFragmentManager();
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            fm.beginTransaction().replace(R.id.content_frame, new EventsMapFragment()).commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
