483c3ffbaf06e96ec87e2c35e7f4a8217550494a
wblachowski <wojciech@blachowski.pl>
2018-11-03 22:04:14 +0100

    Add sports spinner

--- app/src/main/java/com/wblachowski/sportivate/NewEventActivity.java
+++ app/src/main/java/com/wblachowski/sportivate/NewEventActivity.java
@@ -1,7 +1,9 @@
 package com.wblachowski.sportivate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewEventActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class NewEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
         getSupportActionBar().setHomeButtonEnabled(true);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sports, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
