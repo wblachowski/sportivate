4f421c159d084bf28b9bc11388c95ed42ff4d916
wblachowski <wojciech@blachowski.pl>
2018-11-03 16:47:06 +0100

    Add some events

--- app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
+++ app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by wblachowski on 11/3/2018.
@@ -35,6 +36,15 @@ public class EventsMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LatLng putLocation = new LatLng(52.4041498,16.9503366);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(putLocation,15.0f));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(putLocation,16.5f));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4024143,16.949)).title("Mecz piłki nożnej"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.3963452,16.937572)).title("Debel w tenisa"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4057828,16.9278278)).title("Mecz piłki nożnej halowej"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4128263,16.9508353)).title("Mecz piłki nożnej"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4191863,16.9303593)).title("Bieganie - 10km"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4180383,16.9208803)).title("Mecz piłki nożnej"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4061093,16.9548363)).title("Bieganie - 10km"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4107993,16.9743443)).title("Mecz koszykówki"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4099693,16.9387593)).title("Mecz siatkówki"));
     }
 }
