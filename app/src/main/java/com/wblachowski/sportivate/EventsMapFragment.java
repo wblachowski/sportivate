a8ccf5ead2a7c0979c3db6670de07f1347919fef
wblachowski <wojciech@blachowski.pl>
2018-11-03 21:52:50 +0100

    Add colouring markers

--- app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
+++ app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
@@ -19,15 +19,20 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /**
  * Created by wblachowski on 11/3/2018.
  */
 
 public class EventsMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+
+    private ArrayList<Marker> markers = new ArrayList<>();
 
     @Nullable
     @Override
@@ -89,15 +94,15 @@ public class EventsMapFragment extends Fragment implements OnMapReadyCallback, G
 
         LatLng putLocation = new LatLng(52.4041498, 16.9503366);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(putLocation, 16.5f));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4024143, 16.949)).title("Mecz piłki nożnej").snippet("Dziś: 18:00-19:30\nWolnych miejsc: 4"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.3963452, 16.937572)).title("Debel w tenisa").snippet("Jutro: 11:00-12:00\nWolnych miejsc: 1"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4057828, 16.9278278)).title("Mecz piłki nożnej halowej").snippet("Dziś: 15:30-16:30\nWolnych miejsc: 2"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4128263, 16.9508353)).title("Mecz piłki nożnej").snippet("Dziś: 11:00-12:30\nWolnych miejsc: 4"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4191863, 16.9303593)).title("Bieganie - 10km").snippet("Dziś: 18:00-19:00"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4180383, 16.9208803)).title("Mecz piłki nożnej").snippet("Dziś: 12:00-13:30\nWolnych miejsc: 1"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4061093, 16.9548363)).title("Bieganie - 10km").snippet("Dziś: 18:00-19:30"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4107993, 16.9743443)).title("Mecz koszykówki").snippet("Dziś: 20:00-21:30\nWolnych miejsc: 6"));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4099693, 16.9387593)).title("Mecz siatkówki").snippet("Dziś: 15:00-16:00\nWolnych miejsc: 1"));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4024143, 16.949)).title("Mecz piłki nożnej").snippet("Dziś: 18:00-19:30\nWolnych miejsc: 4")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.3963452, 16.937572)).title("Debel w tenisa").snippet("Jutro: 11:00-12:00\nWolnych miejsc: 1")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4057828, 16.9278278)).title("Mecz piłki nożnej halowej").snippet("Dziś: 15:30-16:30\nWolnych miejsc: 2")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4128263, 16.9508353)).title("Mecz piłki nożnej").snippet("Dziś: 11:00-12:30\nWolnych miejsc: 4")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4191863, 16.9303593)).title("Bieganie - 10km").snippet("Dziś: 18:00-19:00")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4180383, 16.9208803)).title("Mecz piłki nożnej").snippet("Dziś: 12:00-13:30\nWolnych miejsc: 1")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4061093, 16.9548363)).title("Bieganie - 10km").snippet("Dziś: 18:00-19:30")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4107993, 16.9743443)).title("Mecz koszykówki").snippet("Dziś: 20:00-21:30\nWolnych miejsc: 6")));
+        markers.add(googleMap.addMarker(new MarkerOptions().position(new LatLng(52.4099693, 16.9387593)).title("Mecz siatkówki").snippet("Dziś: 15:00-16:00\nWolnych miejsc: 1")));
     }
 
     @Override
@@ -106,5 +111,17 @@ public class EventsMapFragment extends Fragment implements OnMapReadyCallback, G
         intent.putExtra("TITLE", marker.getTitle());
         intent.putExtra("SNIPPET", marker.getSnippet());
         startActivity(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for(Marker marker: markers){
+            if(JoinedEvents.events.contains(new Event(marker.getTitle(),marker.getSnippet()))){
+                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            }else{
+                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }
+        }
     }
 }
