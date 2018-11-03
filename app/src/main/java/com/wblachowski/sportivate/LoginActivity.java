2e93769e2ce1de2532aaa9bf6515c3c85c5f2ac2
wblachowski <wojciech@blachowski.pl>
2018-11-03 15:12:00 +0100

    Change sleep to 0.5s

--- app/src/main/java/com/wblachowski/sportivate/LoginActivity.java
+++ app/src/main/java/com/wblachowski/sportivate/LoginActivity.java
@@ -308,7 +308,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             try {
                 // Simulate network access.
-                Thread.sleep(2000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 return false;
             }
