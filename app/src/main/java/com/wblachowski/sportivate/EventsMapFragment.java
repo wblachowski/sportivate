28efe18ea1395aadb467b63b7efca71d1842afe4
wblachowski <wojciech@blachowski.pl>
2018-11-03 16:33:50 +0100

    Change default map location to Piotrowo

--- app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
+++ app/src/main/java/com/wblachowski/sportivate/EventsMapFragment.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by wblachowski on 11/3/2018.
@@ -32,6 +34,7 @@ public class EventsMapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        LatLng putLocation = new LatLng(52.4041498,16.9503366);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(putLocation,15.0f));
     }
 }
