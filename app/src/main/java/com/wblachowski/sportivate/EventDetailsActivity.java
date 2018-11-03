ff6817aea4c243a7d045975951159fff5b2dfe5b
wblachowski <wojciech@blachowski.pl>
2018-11-03 18:16:15 +0100

    Change event details activity

--- app/src/main/java/com/wblachowski/sportivate/EventDetailsActivity.java
+++ app/src/main/java/com/wblachowski/sportivate/EventDetailsActivity.java
@@ -2,6 +2,7 @@ package com.wblachowski.sportivate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class EventDetailsActivity extends AppCompatActivity {
@@ -14,5 +15,13 @@ public class EventDetailsActivity extends AppCompatActivity {
         setTitle(getIntent().getStringExtra("TITLE"));
         TextView snippet = (TextView) findViewById(R.id.eventSnippet);
         snippet.setText(getIntent().getStringExtra("SNIPPET"));
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.eventLayout);
+        String[] participants = {"Jan Kowalski","Adam Bednarek","Zdzisław Zamojcin"};
+        for(String participant : participants){
+            TextView textView = new TextView(this);
+            textView.setText(participant);
+            layout.addView(textView);
+        }
     }
 }
